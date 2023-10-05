08f31ef980e93662a948e429e9e794603c782341
Will Wylie <wyliew@umich.edu>
2023-10-05 01:56:05 -0400

    Finished(?) client side + QUIT; Server still needs more

--- P1client.cpp
+++ P1client.cpp
@@ -3,23 +3,85 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <unistd.h>
+#include <string>
 
 #define PORT 2982
 #define MAX_LINE 256
+const std::string COMMANDS = "BUY SELL LIST BALANCE SHUTDOWN QUIT";
 
 int main(int argc, char* argv[]) {
     // Server connection variables
     struct hostent *hp;
-    int status, valread, client_fd;
+    int status, valread;
     struct sockaddr_in sin;
     char buf[MAX_LINE];
     char* host;
-    int sin_len = sizeof(sin);
+    int s, len;
 
     if (argc == 2) {
         host = argv[1];
     } else {
-        
+        perror("Need host server\n");
+        exit(1);
     }
 
+    hp = gethostbyname(host);
+    if (!hp) {
+        fprintf(stderr, "Unknown host: %s\n", host);
+        exit(1);
+    }
+
+    memset(&sin, 0, sizeof(sin));
+
+    sin.sin_family = AF_INET;
+    memcpy(&sin.sin_addr, hp->h_addr, hp->h_length);
+    sin.sin_port = htons(PORT);
+
+    if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Failed to create a socket\n");
+        exit(1);
+    }
+    
+    if(connect(s, (struct sockaddr*)&sin, sizeof(sin)) < 0) {
+        perror("Failed connection\n");
+        close(s);
+        exit(1);
+    }
+
+    printf("Connected to server: type \"HELP\" for commands and format\n\n");
+
+    while (fgets(buf, sizeof(buf), stdin)) {
+        buf[MAX_LINE - 1] = '\0';
+        len = strlen(buf) + 1;
+
+        char* tmp = (char*)malloc(sizeof(buf)); // temporary variable for use of strtok
+        strcpy(tmp, buf);
+        char* cpy = strtok(tmp, " ");
+        free(tmp);
+        // Check to be sure valid command was sent
+        if (COMMANDS.find(cpy) == std::string::npos) {
+            fprintf(stderr, "400 invalid command\nCommand \"%s\" not recognized\n", buf);
+            continue;
+        }
+        // If client quits
+        if (strcmp(cpy, "QUIT") == 0) {
+            close(s);
+            printf("200 OK");
+            return 0;
+        }
+        // Send command
+        send(s, buf, len, 0);
+        // Receive server feedback
+        valread = recv(s, buf, sizeof(buf), 0);
+        if (valread <= 0) {
+            fprintf(stderr, "Server unexpectedly closed");
+            exit(1);
+        }
+        printf(buf);
+        if (strcmp(cpy, "SHUTDOWN")) {
+            close(s);
+            return 0;
+        }
+    }
 }
--- P1server.cpp
+++ P1server.cpp
@@ -4,17 +4,24 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <sqlite3.h>
+#include <string>
 
 #define PORT 2982
 #define MAX_LINE 256
+#define INV_COM(n) "400 invalid command\n" + n
+#define FORM_ERR(n) "403 message format error\n" + n
 
 int main() {
     
     // Server needed variables
-    int server_fd, new_socket, valread;
     struct sockaddr_in sin;
     char buf[MAX_LINE];
-    int sin_len = sizeof(sin);
+    int s, new_s, valread;
+    socklen_t len = sizeof(sin);
+    memset(&sin, 0, sizeof(sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = INADDR_ANY;
+    sin.sin_port = htons(PORT);
 
     // SQLite needed variables
     sqlite3 *db;
@@ -25,12 +32,77 @@ int main() {
     int rc = sqlite3_open("PokemonDB.db", &db);
     if (rc) {
         fprintf(stderr, "Failed to open: %s\n", sqlite3_errmsg(db));
-        return 1;
+        exit(1);
     }
 
     // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0) < 0) {
-
+    if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Failed to create a socket\n");
+        exit(1);
     }
 
+    if (bind(s, (struct sockaddr*)&sin, sizeof(sin)) < 0) {
+        perror("Failed to bind socket\n");
+        exit(1);
+    }
+
+    listen(s, 1);
+    while(1) {
+        if ((new_s = accept(s, (struct sockaddr*)&sin, &len)) < 0) {
+            perror("Failed to accept client\n");
+            exit(1);
+        }
+
+        
+    }
+}
+
+void buy_request(int client_socket, sqlite3* db, const char* request) {
+    char pokemon_name[50];
+    char card_type[50];
+    char rarity[50];
+    double price_per_card;
+    int count;
+    int owner_id;
+    char response[MAX_LINE];
+    char sql_query[256];
+    char* zErrMsg = 0;
+
+    sscanf(request, "BUY %s %s %s %lf %d %d", pokemon_name, card_type, rarity, &price_per_card, &count, &owner_id);
+
+    // Query the database to get user balance
+    double user_balance;
+    sprintf(sql_query, "SELECT usd_balance FROM Users WHERE ID=%d;", owner_id);
+    
+    // Check if user has enough balance
+    if (user_balance < price_per_card * count) {
+        sprintf(response, "Not enough balance.");
+        send(client_socket, response, strlen(response), 0);
+        return;
+    }
+
+    // Deduct the price from user's balance
+    user_balance -= price_per_card * count;
+    sprintf(sql_query, "UPDATE Users SET usd_balance=%.2lf WHERE ID=%d;", user_balance, owner_id);
+    
+    int rc = sqlite3_exec(db, sql_query, 0, 0, &zErrMsg);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+        return; // Error message
+    }
+
+    // Create a new record in the card’s table
+    sprintf(sql_query, "INSERT INTO Pokemon_Cards (card_name, card_type, rarity, count, owner_id) VALUES ('%s', '%s', '%s', %d, %d);",
+            pokemon_name, card_type, rarity, count, owner_id);
+
+    rc = sqlite3_exec(db, sql_query, 0, 0, &zErrMsg);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+        return; // Error message
+    }
+    // Send a success response
+    sprintf(response, "200 OK\nBOUGHT: New balance: %d %s. User USD balance $%.2lf", count, pokemon_name, user_balance);
+    send(client_socket, response, strlen(response), 0);
 }
